31aaae170eac3c02f23ba327dd1b9d32eaff4550
habnacos <36807577+habnacos@users.noreply.github.com>
2018-03-17 02:43:38 -0500

    Add files via upload

--- habib/script/lista.js
+++ habib/script/lista.js
@@ -1,23 +1,33 @@
 var obtJson;
 
-$getJSON("../json/usuarios.json", function(data) {
-	obtJson = data.items;
-	alert(obtJson);
+
+$.getJSON("../json/usuarios.json", function( data ) {
+    obtJson = data.usuarios;
+    console.log(obtJson);
 })
 
-
 $(document).ready(function () {
-	var tabla = "<table>";
-
-
-	for (var i = 0; i < 10; i++) {
-		tabla += "<tr>"
-		for (var j = 0; j < 5; j++) {
-			tabla += "<td>habib</td>";
+    //$.getJSON("../json/usuarios.json", function( data ) {
+    	var count = 1;
+	    var tabla = "<table class=lista border=1>";
+		for (var i = 0; i<1000; i++) {
+			tabla += "<tr>"
+			for (var j = 1; j <= 5; j++) {
+				switch (j) {
+					case 1:
+						tabla += "<td><input type=checkbox class=voto>"+(count++)+"</td>";
+						break;
+					case 2:
+						tabla += "<td>cedula</td>";
+						break;
+					default:
+						tabla += "<td><input type=text class=texto></td>";
+						break;
+				}
+			}
+			tabla += "</tr>"
 		}
-		tabla += "</tr>"
-	}
-	tabla += "</table>";
-	alert(tabla);
-	$("#box").html(tabla);
+		tabla += "</table>";
+		$("#tabla").html(tabla);
+	//})
 });
