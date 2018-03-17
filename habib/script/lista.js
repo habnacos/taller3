5879ae1b8e02804244e63774294662fab40ffd57
habnacos <36807577+habnacos@users.noreply.github.com>
2018-03-17 03:00:27 -0500

    Add files via upload

--- habib/script/lista.js
+++ habib/script/lista.js
@@ -10,7 +10,9 @@ $(document).ready(function () {
     //$.getJSON("../json/usuarios.json", function( data ) {
     	var count = 1;
 	    var tabla = "<table class=lista border=1>";
-		for (var i = 0; i<1000; i++) {
+	    tabla += "<tr class=encabezado><td rowspan=2>ORD</td><td rowspan=2><b>No. CEDULA</b>VERIFIQUE TODO EL NÚMERO</td><td colspan=3>APELLIDOS Y NOMBRES SEGUN CEDULA</td>";
+	    tabla += "<tr 	class=encabezado><td>PRIMER APELLIDO</td><td>SEGUNDO APELLIDO</td><td>NOMBRES</td>";
+		for (var i = 0; i<10; i++) {
 			tabla += "<tr>"
 			for (var j = 1; j <= 5; j++) {
 				switch (j) {
