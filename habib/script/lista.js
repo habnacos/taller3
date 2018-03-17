c6d638e2200ed3033098f663cbdcf7186dc9d002
habnacos <36807577+habnacos@users.noreply.github.com>
2018-03-17 00:38:23 -0500

    Add files via upload

--- habib/script/lista.js
+++ habib/script/lista.js
@@ -1,12 +1,23 @@
+var obtJson;
+
+$getJSON("../json/usuarios.json", function(data) {
+	obtJson = data.items;
+	alert(obtJson);
+})
+
+
 $(document).ready(function () {
 	var tabla = "<table>";
+
+
 	for (var i = 0; i < 10; i++) {
 		tabla += "<tr>"
-		for (var i = 0; i < 5; i++) {
+		for (var j = 0; j < 5; j++) {
 			tabla += "<td>habib</td>";
 		}
 		tabla += "</tr>"
 	}
 	tabla += "</table>";
+	alert(tabla);
 	$("#box").html(tabla);
 });
